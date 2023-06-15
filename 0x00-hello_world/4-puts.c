69e5c382efbcbdac635f1b70d9c8b138d6a43f9b
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-15 06:48:50 -0700

    updated 4-puts.c x2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
+/*This function will print the statement in puts*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-
-
+	return (0);
 }
