4f569ef31bb7a8b300be0f73c35d9d77754049df
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-15 08:47:59 -0700

    created 6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - execute the printf function which prints the size of all variables
+ *
+ * Return: will return 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of an long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of an long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of an float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
+
