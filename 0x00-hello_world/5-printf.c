e7e42e85e433a9f746b5e81eff9e51598b08c037
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-15 08:22:15 -0700

    update 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - execute the printf function
  *
- * return: will return 0
+ * Return: will return 0
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /* Return value indicates successful program execution */
 }
