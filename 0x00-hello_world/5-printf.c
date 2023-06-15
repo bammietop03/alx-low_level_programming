9d57c4c424066235289d29cfe4098fc8b31de0ab
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-15 07:46:22 -0700

    created 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - execute the function which prints the value in printf
+ *
+ * retun: return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	retun (0);
+}
