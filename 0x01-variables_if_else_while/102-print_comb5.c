33c6eabc7020f52b2a2fee957ee705f0d50e7554
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-16 10:43:24 -0700

    hopefully this works

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,23 @@ int main(void)
 {
 	int num;
 	int num2;
-	int num3;
-	int num4;
 
-	for (num = 48; num < 58; num++)
+	for (num = 0; num < 100; num++)
 	{
-		for (num2 = 48; num2 < 57; num2++)
+		for (num2 = 0; num2 < 100; num2++)
 		{
-			for (num3 = 48; num3 < 58; num3++)
+			if (num2 > num)
 			{
-				for (num4 = 48; num4 < 58; num4++)
-				{
-					putchar(num);
-					putchar(num2);
-					putchar(' ');
-					putchar(num3);
-					putchar(num4);
+				putchar((num / 10) + 48);
+				putchar((num % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
 
-					if (num != 57 || num2 != 56 || num3 != 57 || num4 != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				if (num != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
