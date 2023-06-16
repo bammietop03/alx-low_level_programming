984f6ac546d3fd5c8a122ce11b61c8e874af9642
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-16 06:56:43 -0700

    created 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints number 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
