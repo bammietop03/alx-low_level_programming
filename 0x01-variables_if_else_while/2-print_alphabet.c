0cc1b974f5c17d0c6fe0a9404c0a72ce3f59a5e9
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-16 05:29:01 -0700

    created print alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+
+	{
+		putchar(letters);
+	}
+
+	return (0);
+}
