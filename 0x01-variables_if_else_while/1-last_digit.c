13ec03df5b3be5f06c40bfc470b8caef8010a949
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-16 05:04:25 -0700

    update lst digit x2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,9 +16,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastnum = n % 10;
-	if (n > 5, lastnum != 0)
+	if (n > 5 && lastnum != 0)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastnum);
-	else if (n < 6, lastnum != 0)
+	else if (n < 6 && lastnum != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
 	else if (lastnum == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lastnum);
