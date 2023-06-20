21dec6291dea1c736f8cf48ed36b19db6c077e8a
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-20 04:13:34 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - execut
+ * main - execute the print_alphabet function
  *
  * Return: Always 0 (Success)
  */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - prints all alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	for (; letter <= 'z'; letter++)
+
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
