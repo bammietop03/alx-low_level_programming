9052d90310f31c711e3590d7e40ecf0ce053348c
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-20 13:37:43 -0700

    created 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @x: parameters for add function
+ * @y: parameter for add function
+ * Return: r
+ */
+
+int add(int x, int y)
+{
+
+	int z = x + y;
+
+	return (z);
+}
