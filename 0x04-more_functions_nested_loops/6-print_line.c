dbd5dbb857f567eca86c87c49a9433d809b66ba5
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-22 13:34:16 -0700

    testing

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints '_' the amount of n variable
+ * @n: is the parameter that will take the argument
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+	if (n <= 0)
+		_putchar('\n');
+
+	_putchar('\n')i;
+}
