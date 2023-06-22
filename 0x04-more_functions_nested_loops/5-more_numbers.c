f320c53483f1bb0f9494f6e528d0fcefd26d9afd
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-22 13:13:36 -0700

    Created 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints all numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int num, times;
+
+	for (times = 1; times <= 10; times++)
+	{
+
+		for (num = 0; num <= 14; num++)
+		{
+
+			if (num >= 10)
+				_putchar((num / 10) + 48);
+
+			_putchar((num % 10) + 48;
+		}
+		_putchar('\n');
+	}
+}
