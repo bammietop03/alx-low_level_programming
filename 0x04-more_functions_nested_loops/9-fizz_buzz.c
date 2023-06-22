585545cbfb19c067269080ee9ad35c3bf8f2ddff
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-22 14:43:06 -0700

    Created 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints 1 - 100 and prints fizz if the number is divisible by 3
+ * and buzz when it is divisible by 5
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+
+		else if (i % 5 == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", i);
+	}
+	return (0);
+}
