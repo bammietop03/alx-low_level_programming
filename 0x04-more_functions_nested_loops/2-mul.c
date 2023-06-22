54d7eeeb17bb1a75c88b974adb393f377cf48920
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-22 12:34:32 -0700

    Created 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* mul - multiply two integers
+* @a: is the first parameter that takes the argument
+* @b: is the second parameter that takes the argument
+*
+* Return: c
+*/
+
+int mul(int a, int b)
+{
+
+	int c;
+
+	c = a * b;
+	return (c);
+}
