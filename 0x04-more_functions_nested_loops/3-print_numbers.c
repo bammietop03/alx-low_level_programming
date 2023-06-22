2a6cfa785dd6de2342c02d848e1c8b2ee5623dc5
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-22 12:40:56 -0700

    Created 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_numbers - prints number 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
