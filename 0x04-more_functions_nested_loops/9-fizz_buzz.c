8b8337b550251aaf7701f13d81143f00fadee682
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-22 15:37:10 -0700

    finaly

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,28 @@
  */
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	for (; i <= 100; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			if (i % 3 == 0)
-			{
-				printf("Fizz");
-			}
-			if (i % 5 == 0)
-			{
-				printf("Buzz");
-			}
-		} else
-		{
+
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz");
+
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz");
+
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+
+		else
 			printf("%d", i);
-		}
+
 		if (i != 100)
 			printf(" ");
 	}
+
 	printf("\n");
+
 	return (0);
 }
