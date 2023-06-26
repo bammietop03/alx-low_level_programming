0eaaa25673de4c0feaa6149f2e36267bb507773a
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-26 02:35:53 -0700

    Created 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - Resets the value of n to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory is for pointers, array and strings
