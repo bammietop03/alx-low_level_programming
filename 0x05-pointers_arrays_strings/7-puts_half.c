6d5854352e67e88b074f89e7ab387c8cfb493f27
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-26 07:23:12 -0700

    Created 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: is a pointer variable that accepts a string as argument
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	if (len % 2 == 0)
+	{
+		i = len / 2;
+
+		while (i <= len - 1)
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+	}
+
+	else if (len % 2 != 0)
+	{
+		i = (len - 1) / 2;
+		while (i <= len - 1)
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+	}
+	_putchar('\n');
+}
