920301e13470dfcd339da91c6cff8a780ee9cfef
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-26 03:14:11 -0700

    Created 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - prints the lenght of pointers variable pointing to a string
+ *
+ * @s: is a variable that accept a string
+ * Return: returns the value of lenght
+ */
+
+int _strlen(char *s)
+{
+	int lenght = strlen(s);
+
+	return (lenght);
+}
