92ca0f9a09bea2f3b348e6b7249f755c0a8a5377
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-29 15:06:17 -0700

    Created 101-print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers with _putchar
+ *
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	_putchar('0' + (n % 10));
+}
