beeb95d53291dd4e3d50ed0df47e74e5655e2938
Bamiwo Adebayo <bammietop@gmail.com>
2023-06-29 14:17:03 -0700

    Created

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @str: string to be capitalized
+ * Return: @str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+			str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
+			str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
+			str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+			str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (islower(str[i]))
+				str[i] = toupper(str[i]);
+		}
+		else
+		{
+			if (isupper(str[i]))
+				str[i] = tolower(str[i]);
+		}
+		i++;
+	}
+
+	return (str);
+}
