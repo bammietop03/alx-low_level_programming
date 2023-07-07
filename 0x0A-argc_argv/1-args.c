dab508723bf8961f7d4d473d26fcafbe62a5396e
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-07 13:17:04 +0100

    Created 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ *
+ * @argc: count of the arguments supplied to the program
+ * @argv: is an array of pointers to the strings which are those arguments
+ * Return: 0
+ */
+
+int main(int argc,  char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
