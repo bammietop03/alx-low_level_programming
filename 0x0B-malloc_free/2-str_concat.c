250a5ba1ab343a942d9b84b246b4fe514f33c767
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-11 02:15:45 +0100

    Created 2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	char *concat;
 	unsigned int i, j, len1, len2;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	len1 = strlen(s1);
