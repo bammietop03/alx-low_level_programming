7ad465f5eec9d4fb664d372ed3f2f19958d608da
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-13 22:59:05 +0100

    more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
-	unsigned int copy;
+
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -26,19 +26,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if (ptr == NULL)
-		return (malloc(new_size));
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		free(ptr);
+		return (new_ptr);
+	}
 
 	new_ptr = realloc(ptr, new_size);
 
 	if (new_ptr == NULL)
 		return (NULL);
-
-	if (old_size < new_size)
-		copy = old_size;
-	else
-		copy = new_size;
-
-	memcpy(new_ptr, ptr, copy);
 
 	free(ptr);
 
