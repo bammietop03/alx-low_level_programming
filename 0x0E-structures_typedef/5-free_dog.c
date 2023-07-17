c6d0a6fd59b85ed282e44f38d206a6b35423d189
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-17 16:47:52 +0100

    Structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d: contains dog info
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
