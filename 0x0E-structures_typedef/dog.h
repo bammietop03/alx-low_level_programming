98c904b59ce370f40301572bf3caa5ca019d0530
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-17 13:38:12 +0100

    Structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
