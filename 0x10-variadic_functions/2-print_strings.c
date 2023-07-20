bbb0153af0db2afae54205b10238bd171ca5406e
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-20 15:27:51 +0100

    Variadic Functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", x);
 		}
 		else
-			printf("(nill)");
+			printf("(nil)");
 
 		if (separator && i != n - 1)
 			printf("%s", separator);
