f5658d4aa4ac08a69fc2494c10ebb0f657d925a6
Bamiwo Adebayo <bammietop@gmail.com>
2023-07-20 16:00:18 +0100

    Variadic Function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,7 +33,10 @@ void print_all(const char * const format, ...)
 			{
 				str = va_arg(args, char *);
 				if (str == NULL)
+				{
 					printf("(nil)");
+					break;
+				}
 				printf("%s%s", separator, str);
 				break;
 			}
