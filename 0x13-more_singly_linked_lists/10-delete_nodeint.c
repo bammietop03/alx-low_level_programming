7c76c34e2e63ad285739f04ffb7d82aa6b9d2c8b
bammietop03 <bammietop@gmail.com>
2023-08-01 19:32:47 +0100

    Singly Linked List

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,8 +15,8 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *ptr, *curr;
-
-	if (!head || !*head)
+	
+	if (!*head)
 		return (-1);
 
 	ptr = *head;
@@ -27,6 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = curr->next;
 		free(curr);
 		curr = NULL;
+		return (1);
 	}
 	else
 	{
