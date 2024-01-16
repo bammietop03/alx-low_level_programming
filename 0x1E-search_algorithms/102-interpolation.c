b8f3117fdde7cc09d9b6ea5a08cf2d8cbc28ce5c
Bamiwo Adebayo <bammietop@gmail.com>
2024-01-16 20:05:28 +0100

    Search algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -40,6 +40,6 @@ int interpolation_search(int *array, size_t size, int value)
 
 	pos = low + (((double)(high - low) / (array[high] - array[low]))
 			* (value - array[low]));
-	printf("Value checked array[%d] is out of range\n", pos);
+	printf("Value checked array[%ld] is out of range\n", pos);
 	return (-1);
 }
